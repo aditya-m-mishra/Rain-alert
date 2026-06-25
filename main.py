2b15bbc1e1b0bcdaa221827b32872ab2a524bdf6
aditya-m-mishra <adiapplies@gmail.com>
2026-06-25 12:30:09 +0530

    Rain check condition added

--- main.py
+++ main.py
@@ -6,10 +6,23 @@ api_key = '29c88146bfaf49e2964447caefeb87c2'
 weather_params = {
     "lat": 30.704649,
     "lon": 76.717873,
-    "appid": api_key
+    "appid": api_key,
+    "cnt": 4,
 }
 
 response = requests.get(OWM_ENDPOINT, params=weather_params)
+
+
+response.raise_for_status()
 print(response.status_code)
 
-print(response.json())
+weather_data = response.json()
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = (hour_data["weather"][0]["id"])
+    if int(condition_code)<700:
+        will_rain = True
+
+if will_rain:
+    print("bring an umbrella")
