eb7d93b3d52be4b242ca26c8c7d0c2be40b4e3aa
aditya-m-mishra <adiapplies@gmail.com>
2026-06-25 11:53:18 +0530

    API request  called and JSON printed

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-api_key = '29c88146bfaf49e2964447caefeb87c2'  
+import requests
+
+OWM_ENDPOINT = "http://api.openweathermap.org/data/2.5/forecast"
+api_key = '29c88146bfaf49e2964447caefeb87c2'
+
+weather_params = {
+    "lat": 30.704649,
+    "lon": 76.717873,
+    "appid": api_key
+}
+
+response = requests.get(OWM_ENDPOINT, params=weather_params)
+print(response.status_code)
+
+print(response.json())
